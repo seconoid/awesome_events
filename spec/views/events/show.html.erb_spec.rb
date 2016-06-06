d842af182df7e2d4f18486241b6ad462d6cc93c9
seconoid <seconsecond@gmail.com>
2016-06-06 18:12:38 +0900

    Update events#show view test

---
+++ spec/views/events/show.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "events/show" do
+  context '未ログインユーザがアクセスしたとき' do
+    before do
+      def view.logged_in?
+      end
+
+      def view.current_user
+      end
+
+      allow(view).to receive(:logged_in?) { false }
+      allow(view).to receive(:current_user) { nil}
+    end
+
+    context 'かつ @event.owner がnil のとき' do
+      before do
+        assign(:event, create(:event, owner: nil))
+        assign(:tickets, [])
+      end
+
+      it '"退会したユーザです"と表示されていること' do
+        render
+        expect(rendered).to match /退会したユーザです/
+      end
+    end
+  end
+end
